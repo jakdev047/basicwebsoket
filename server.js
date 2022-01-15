aa7431e7cd204591a0a0b4c9ccb0a886628da498
jakdev047 <jakdevfreelancer047@gmail.com>
2022-01-15 23:45:20 +0600

    basic socket.io configure

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const  http= require('http');
+const http = require("http");
 const expressServer = http.createServer(app);
 
+// soket configure
+const { Server } = require("socket.io");
+const io = new Server(expressServer);
+
+io.on("connection", (socket) => {
+  console.log("New User Connected");
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
+});
 
 // PORT
 const PORT = process.env.PORT || 8080;
 
 // routes
-const indexRoute = require('./routes');
-app.use('/', indexRoute);
+const indexRoute = require("./routes");
+app.use("/", indexRoute);
 
 // listen
-expressServer.listen(PORT,()=>{
-    console.log(`${PORT} Server is Running…`);
+expressServer.listen(PORT, () => {
+  console.log(`${PORT} Server is Running…`);
 });
-

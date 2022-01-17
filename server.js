adec4d5dad6ba0a07fa7ed6a170c6118e139ab65
jakdev047 <jakdevfreelancer047@gmail.com>
2022-01-17 21:50:09 +0600

    basic chat application

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express();
 const http = require("http");
 const expressServer = http.createServer(app);
 
+// PORT
+const PORT = process.env.PORT || 8080;
+
+// routes
+const indexRoute = require("./routes");
+app.use("/", indexRoute);
+
 // soket configure
 const { Server } = require("socket.io");
 const io = new Server(expressServer);
@@ -10,34 +17,34 @@ const io = new Server(expressServer);
 io.on("connection", (socket) => {
   console.log("New User Connected");
 
-  // reserve event
-  setTimeout( function() {
-      socket.send('Data Transfer server to client');
-  },500000);
+  /*
+    // reserve event
+    setTimeout( function() {
+        socket.send('Data Transfer server to client');
+    },500000);
 
-  // custom event
-  setTimeout( function() {
-    socket.emit('customEvent','This is Custom Event');
-  },500000);
+    // custom event
+    setTimeout( function() {
+      socket.emit('customEvent','This is Custom Event');
+    },500000);
 
-  // data receive from client
-  setTimeout( function() {
-    socket.on('message',function(msg){
-      console.log(msg);
+    // data receive from client
+    setTimeout( function() {
+      socket.on('message',function(msg){
+        console.log(msg)
+      });
+    },1000);
+  */
+    // data receive from client
+    socket.on('customEvent',function(msg){
+      // data send to Client
+      io.emit('chattingDataShow',msg)
     });
-  },1000);
 
   socket.on("disconnect", () => {
     console.log("User Disconnected");
   });
 });
-
-// PORT
-const PORT = process.env.PORT || 8080;
-
-// routes
-const indexRoute = require("./routes");
-app.use("/", indexRoute);
 
 // listen
 expressServer.listen(PORT, () => {
